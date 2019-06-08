befe6e1b14ce7e9a32cd7350bb4141f70fb4d3a4
Wuying <goeiwy@163.com>
2019-06-08 22:29:52 +0800

    文件和图片上传step1

---
+++ src/main/java/com/controller/PictureController.java
@@ -0,0 +1,17 @@
+package com.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+@RequestMapping("/picture")
+public class PictureController {
+
+    @RequestMapping(value = "/init",method = RequestMethod.GET)
+    public ModelAndView go(){
+        return new ModelAndView("picture");
+    }
+}
