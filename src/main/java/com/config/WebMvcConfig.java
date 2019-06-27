cb22093723aed8098044d618816c4d1160541bfc
milk <1592053521@qq.com>
2019-06-27 09:52:41 +0900

    拦截器注册完成

--- src/main/java/com/config/WebMvcConfig.java
+++ src/main/java/com/config/WebMvcConfig.java
@@ -1,6 +1,9 @@
 package com.config;
 
+import com.utils.AuthUtil;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -10,6 +13,19 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/pic/**").addResourceLocations("file:D://pic/");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInterceptor()).addPathPatterns("/**");
+    }
+
+    /**
+     * 初始化用户登录验证拦截器
+     */
+    @Bean
+    public AuthUtil authInterceptor(){
+        return new AuthUtil();
     }
 
 }
--- src/main/java/com/utils/AuthUtil.java
+++ src/main/java/com/utils/AuthUtil.java
@@ -1,24 +1,31 @@
 package com.utils;
 
 import com.db.tables.pojos.MUsers;
+import com.interceptor.Auth;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.lang.reflect.Method;
 
 @Slf4j
 public class AuthUtil implements HandlerInterceptor {
     @Override
-    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object handler) throws Exception {
         log.info("======================是否登录验证开始===============");
-        HttpSession session = httpServletRequest.getSession();
-        MUsers users = CommonUtil.getLoginUser(session);
-        if(users == null){
-            httpServletResponse.sendRedirect(httpServletRequest.getContextPath()+"/login/index");
-            return false;
+        HandlerMethod handlerMethod = (HandlerMethod) handler;
+        Method method = handlerMethod.getMethod();
+        if(method.isAnnotationPresent(Auth.class)){
+          HttpSession session = httpServletRequest.getSession();
+          MUsers users = CommonUtil.getLoginUser(session);
+          if(users == null){
+              httpServletResponse.sendRedirect(httpServletRequest.getContextPath()+"/login/index");
+              return false;
+          }
         }
         return true;
     }
